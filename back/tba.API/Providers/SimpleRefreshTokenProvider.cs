6f3a8f337d5893cd7ca5f84c3703dc7209e89654
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2017-03-25 22:50:10 +0300

    creating refresh token

---
+++ back/tba.API/Providers/SimpleRefreshTokenProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security.Infrastructure;
+using tba.Common.Helpers;
+using tba.Model;
+
+namespace tba.API.Providers
+{
+    public class SimpleRefreshTokenProvider : IAuthenticationTokenProvider
+    {
+        public void Create(AuthenticationTokenCreateContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task CreateAsync(AuthenticationTokenCreateContext context)
+        {
+            var clientid = context.Ticket.Properties.Dictionary["as:client_id"];
+
+            if (string.IsNullOrEmpty(clientid))
+                return;
+
+            var refreshTokenId = Guid.NewGuid().ToString("n");
+
+            using (var _repo = new AuthRepository())
+            {
+                var refreshTokenLifeTime = context.OwinContext.Get<string>("as:clientRefreshTokenLifeTime");
+
+                var token = new RefreshToken
+                {
+                    Id = Crypto.GetHash(refreshTokenId),
+                    ClientId = clientid,
+                    Subject = context.Ticket.Identity.Name,
+                    IssuedUtc = DateTime.UtcNow,
+                    ExpiresUtc = DateTime.UtcNow.AddMinutes(Convert.ToDouble(refreshTokenLifeTime))
+                };
+
+                context.Ticket.Properties.IssuedUtc = token.IssuedUtc;
+                context.Ticket.Properties.ExpiresUtc = token.ExpiresUtc;
+
+                token.ProtectedTicket = context.SerializeTicket();
+
+                var result = _repo.AddRefreshToken(token);
+
+                if (result)
+                    context.SetToken(refreshTokenId);
+            }
+        }
+
+        public void Receive(AuthenticationTokenReceiveContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ReceiveAsync(AuthenticationTokenReceiveContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
